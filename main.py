0e7eb60eb44c3cf49749c6c0dc6dd1fc12d01fef
Свиридов Фёдор <avrora013@mail.ru>
2023-04-06 21:48:42 +0300

    v 1.0

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from virtual import ExprEnum, decoder
+
+
+def main():
+    code = [ExprEnum.PUSH, 22, ExprEnum.PUSH, 45, ExprEnum.RAM, ExprEnum.SUB, ExprEnum.POP, ExprEnum.PUSH, 23,
+             ExprEnum.PUSH, 9, ExprEnum.PUSH, 5, ExprEnum.RAM, ExprEnum.PRINT, ExprEnum.ADD, ExprEnum.POP,
+             ExprEnum.PUSH, 7, ExprEnum.PUSH, 7, ExprEnum.RAM, ExprEnum.ADD, ExprEnum.POP, ExprEnum.POP,
+             ExprEnum.PRINT, ExprEnum.EXIT]
+    ip = 0
+    while ip < len(code):
+        # Переменная, которая контролирует работу VM
+        decoder(code[ip], code)
+        ip += 1
+
+
+if __name__ == "__main__":
+    main()
